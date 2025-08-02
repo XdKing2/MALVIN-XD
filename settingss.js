5280fb9a0bd3b935a1f07497880e390b412d232d
SOUMYADIP-gff <sh6179155@gmail.com>
2025-08-02 13:37:32 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: " "malvin~JukhVTyB#z2RjMTNxx-ze1QfVqnbLv7tb0MQNYUHDAVWFB1-t5D4,  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
