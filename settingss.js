398e1ff93e1aaab0c040a79759e8196cd952f625
Bin5z <edsonbenglo010@gmail.com>
2025-08-15 02:27:29 -0400

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "malvin~V2tCkLpZ#oJwRNYFMg3sUgcQbtMcDx5QNbN0ls7EHZQA6bEhx_9E",  // add your Session Id here
+  SESSION_ID: "",  // add your Session Id here
   
-  OWNER_NUMBER: "50943472523", // put your phone number here
+  OWNER_NUMBER: "+50943472523", // put your phone number here
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: "!", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Haïti/Saint-Marc" //put your country timeZone....leave blank if u don't know.
 };
