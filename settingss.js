b4d9b7758eb57efe40a0a7e516ee8334ec78f505
Bin5z <edsonbenglo010@gmail.com>
2025-08-15 02:35:45 -0400

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~ty8A0DyT#7ps65OayKzetxyNgWRAaSGX2sYG_NMbRtb6A_bSlC9A",  // add your Session Id here
   
   OWNER_NUMBER: "+50943472523", // put your phone number here
   
