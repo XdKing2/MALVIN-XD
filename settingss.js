f3ad7cac82723525a8354e7926e29896df70aa2e
Piihu2025 <bhagyapiihu@gmail.com>
2025-08-23 01:38:24 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~V78DWLgZ#-sAQvgvbbvcX5vxpWZ6QW21kgY3G2JDmN5Cgnu2Xnmk",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Jakarta, Indonesia (GMT+7)" //put your country timeZone....leave blank if u don't know.
 };
