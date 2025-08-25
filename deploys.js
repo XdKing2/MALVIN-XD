5bb6673f3e68b0e4fa9374990f388a114213bf48
Tinoe2 <lungalee599@gmail.com>
2025-08-25 10:56:56 +0200

    Deploy.js
    Fell

---
+++ deploys.js
@@ -0,0 +1,12 @@
++ Project Status: Active
+! Version: V.1.5.0 Neon Edition
+# License: MIT
+module.exports = {
+  SESSION_ID: "malvin~92EBnKgI#ArvU-pFWV9pNzSrzQXYkQrzZLI-PUysecitKlGp4pDU",  // add your Session Id here
+  
+  OWNER_NUMBER: "263787856324", // put your phone number here
+  
+  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  
+  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+};
