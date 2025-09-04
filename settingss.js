23c77589d6b9a2dbfa6163ff1775d6d4ef064afd
louishans158-ctrl <louishans158@gmail.com>
2025-09-03 22:22:16 -0400

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~Avc3zZTS#h91R3rQi-UUsQHXudODqtDQqY30xK1_hDZ6daEayRGE",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "50935042043", // put your phone number here
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: "•", // prefix (e.g., ., /, !, *)
   
   TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
 };
