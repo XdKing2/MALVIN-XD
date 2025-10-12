f9ab236ac12e469f6f2e413f3de136cf4a9719ca
Lee5526 <150006591+Lee5526@users.noreply.github.com>
2025-10-12 12:33:46 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~5rNSVAzS#RFdp01Xf9sVnlzo1eLAc8bxnthJgoi1OkKNg8MkbnhQ970b706506eba94c",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "254721411618", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Africa/Nairobi" //put your country timeZone....leave blank if u don't know.
 };
